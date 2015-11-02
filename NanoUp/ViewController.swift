fe4e0b4b958036e059295530cd3ce964fa730de8
Ricky Ayoub <vgmoose@gmail.com>
2015-11-02 01:26:31 -0500

    Initial Commit

---
+++ NanoUp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NanoUp
+//
+//  Created by Kara on 11/2/15.
+//  Copyright © 2015 Ricky Ayoub. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
